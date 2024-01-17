b4185f37134382f4e940f2ed75a36248b75efb43
Cevdet Tufan <cevdet.tufan@hotmail.com>
2024-01-17 20:12:10 +0300

    #1 - Created the project.

---
+++ WordReminderAppMaui/WordReminderAppMaui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WordReminderAppMaui
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new AppShell();
+		}
+	}
+}
---
+++ WordReminderAppMaui/WordReminderAppMaui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WordReminderAppMaui
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
